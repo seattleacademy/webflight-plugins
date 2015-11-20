864bf2661f042482b96a7637185f13158b642d10
Gary Anderson <git@garyvictoranderson.com>
2015-11-20 08:29:38 -0800

    animateLed made from animations

--- animateLeds/public/js/animations.js
+++ animateLeds/public/js/animations.js
@@ -1,14 +1,6 @@
-PILOT_ACCELERATION = 0.04;
-
 (function(window, document, $, undefined) {
     'use strict';
 
-
-
-
-    // on press, replace current buttons with new one
-    // remove unpress
-    // set timeout after press to cancel (or stop) after so many seconds
 
     /*
      * Constructuor
@@ -20,14 +12,8 @@ PILOT_ACCELERATION = 0.04;
 
         //$('#controls').append('<span id="animations">...</span>');
         $('.main-container .wrapper').append("<div id='animations'>" + "</div>");
-        $('#animations').append('<li id="phiM30Deg">phiM30Deg 1000</li>');
-        $('#animations').append('<li id="turnaround">turnaround 5000</li>');
-        $('#animations').append('<li id="wave">wave 5000</li>');
-        $('#animations').append('<li id="flipAhead">flipAhead 15</li>');
-        $('#animations').append('<li id="flipBehind">flipBehind 15</li>');
-        $('#animations').append('<li id="flipLeft">flipLeft 15</li>');
-        $('#animations').append('<li id="flipRight">flipRight 15</li>');
-
+        $('#animations').append('<li id="blinkGreenRed">blinkGreenRed</li>');
+        $('#animations').append('<li id="blinkGreen">blinkGreen</li>');
 
         // Register the various event handlers
         this.listen();
@@ -42,48 +28,19 @@ PILOT_ACCELERATION = 0.04;
         var animations = this;
 
 
-        $('#phiM30Deg').click(function(ev) {
-            console.log('phiM30Deg clicked');
+        $('#blinkGreenRed').click(function(ev) {
+            console.log('blinkGreenRed clicked');
             //ev.preventDefault();
-            animations.animate('phiM30Deg', 1000);
+            animations.animate('blinkGreenRed', 2, 3);
         });
 
-        $('#turnaround').click(function(ev) {
-            console.log('turnaround clicked');
+        $('#blinkGreen').click(function(ev) {
+            console.log('blinkGreen');
             //ev.preventDefault();
-            animations.animate('turnaround', 5000);
+            animations.animate('blinkGreen', 2, 3);
         });
 
-        $('#wave').click(function(ev) {
-            console.log('wave clicked');
-            //ev.preventDefault();
-            animations.animate('wave', 5000);
-        });
 
-        $('#flipAhead').click(function(ev) {
-            console.log('flipAhead clicked');
-            //ev.preventDefault();
-            animations.animate('flipAhead', 15);
-        });
-        $('#flipBehind').click(function(ev) {
-            console.log('flipBehind clicked');
-            //ev.preventDefault();
-            animations.animate('flipBehind', 15);
-        });
-        $('#flipLeft').click(function(ev) {
-            console.log('flipLeft clicked');
-            //ev.preventDefault();
-            animations.animate('flipLeft', 15);
-        });
-        $('#flipRight').click(function(ev) {
-            console.log('flipRight clicked');
-            //ev.preventDefault();
-            animations.animate('flipRight', 15);
-        });
-        // $('#ftrim').click(function(ev) {
-        //   ev.preventDefault();
-        //   pilot.ftrim();
-        // });
 
     };
 
