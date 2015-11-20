fa1f73771a75f0d1eb3607e5b4cd987763e18bf9
erinclack <erinclack@seattleacademy.org>
2015-11-20 09:45:58 -0800

    add light procedure

--- animateLeds/index.js
+++ animateLeds/index.js
@@ -3,7 +3,55 @@ function animations(name, deps) {
         socket.on('/animations/animate', function (cmd) {
         	var client = deps.client;
 
-            
+        //      $('#rightMissile').click(function(ev) {
+        //     console.log('rightMissile clicked');
+        //     //ev.preventDefault();
+        //     animations.animate('rightMissile', 2, 3);
+        // });
+ 
+ 			  $('frontLeftGreenOthersRed').click(function(ev) {
+            console.log('frontLeftGreenOthersRed');
+            //ev.preventDefault();
+            animations.animate('frontLeftGreenOthersRed', 2, 3);
+        });
+ 			  $('frontRightGreenOthersRed').click(function(ev) {
+ 			console.log('frontRightGreenOthersRed');
+ 			animations.animate('frontRightGreenOthersRed', 2, 3);	
+
+ 			  });
+				 $('rearRightGreenOthersRed').click(function(ev) {
+ 			console.log('frontRightGreenOthersRed');
+ 			animations.animate('frontRightGreenOthersRed', 2, 3);	
+
+ 			  }); 
+
+ 				$('rearLeftGreenOthersRed').click(function(ev) {
+ 			console.log('frontRightGreenOthersRed');
+ 			animations.animate('frontRightGreenOthersRed', 2, 3);	
+
+ 			  });
+           
+            $('frontLeftGreenOthersRed').click(function(ev) {
+            console.log('frontLeftGreenOthersRed');
+            //ev.preventDefault();
+            animations.animate('frontLeftGreenOthersRed', 2, 3);
+        });
+ 			  $('frontRightGreenOthersRed').click(function(ev) {
+ 			console.log('frontRightGreenOthersRed');
+ 			animations.animate('frontRightGreenOthersRed', 2, 3);	
+
+ 			  });
+				 $('rearRightGreenOthersRed').click(function(ev) {
+ 			console.log('frontRightGreenOthersRed');
+ 			animations.animate('frontRightGreenOthersRed', 2, 3);	
+
+ 			  }); 
+
+ 				$('rearLeftGreenOthersRed').click(function(ev) {
+ 			console.log('frontRightGreenOthersRed');
+ 			animations.animate('frontRightGreenOthersRed', 2, 3);	
+
+ 			  });
             console.log("animate", cmd, cmd.name, cmd.duration);
             //duration = cmd.duration || 15;
             client.animateLeds(cmd.name);
